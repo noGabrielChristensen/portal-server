54acd04f4469c9351c33bce23f0f8f8dda174776
Gabriel Christensen <nogabrielchristensen@gmail.com>
2025-10-04 06:50:49 -0700

    Update script.js

--- Public/script.js
+++ Public/script.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'YOUR_SUPABASE_URL';
 const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
